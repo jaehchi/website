ec9ae85fc04de8bf98c3f6101095fa3fc537b256
Jaehyun Chi <jaehchi@gmail.com>
2018-05-03 22:25:02 -0700

    [add] - deleting commented code

--- client/src/components/Skills/index.jsx
+++ client/src/components/Skills/index.jsx
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import { Progress } from 'react-sweet-progress';
-import "react-sweet-progress/lib/style.css";
-
-import CircularProgressbar from 'react-circular-progressbar';
 import ChangingProgressbar from './ChangingProgressbar';
-import 'react-circular-progressbar/dist/styles.css';
-
 
 import react from '../../../public/images/react.svg';
 import angular from '../../../public/images/angular.svg';
@@ -21,7 +15,6 @@ import redux from '../../../public/images/redux.svg';
 import webpack from '../../../public/images/webpack.svg';
 
 import './skills.scss';
-
 
 class Skills extends Component {
   constructor(props) {
@@ -260,40 +253,3 @@ class Skills extends Component {
 }
 
 export default Skills;
-
-
-
-// <div className="container">
-// <div className="skills-content">
-//   <div   onMouseLeave={() => this.onHoverLeave('react')}  onMouseEnter={ () => this.onHover('react')}>
-//     {
-//       !this.state.react ?  <img src={reactlogo} /> : 
-
-//       <ChangingProgressbar
-//       percentages={[0, 20, 40, 60, 80, 100]}
-//       value="react" 
-//       stylesForPercentage={(percentage) => {
-//         const alpha = (100 + percentage) / 200;
-//         return {
-//           path: {
-//             stroke: `rgba(62, 152, 199, ${alpha})`,
-//           },
-//         };
-//       }}
-//     />
-//     <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-//     <div style={{ position: 'absolute', width: '100%' }}>
-//       <CircularProgressbar
-//         percentage={50}
-//         textForPercentage={null}
-//       />
-//     </div>
-//     <div style={{ width: '80%', padding: '10%' }}>
-//       <img style={{ width: '100%' }} src="https://i.imgur.com/b9NyUGm.png" alt="doge" />
-//     </div>
-//   </div>
-//     }
-
-//   </div>
-// </div>
-// </div> 
