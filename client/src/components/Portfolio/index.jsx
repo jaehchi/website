e40be12d92c06abe84bcc44eaf36f7c53dab1dd8
Jaehyun Chi <jaehchi@gmail.com>
2018-05-08 19:47:15 -0700

    [add] - adding portfolio html/scss

--- client/src/components/Portfolio/index.jsx
+++ client/src/components/Portfolio/index.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+import cits from '../../../public/images/cits.png';
+import codesling from '../../../public/images/codesling.png';
+import bonebuds from '../../../public/images/bonebuds.png';
+import rps from '../../../public/images/rps.png';
+import tictactoe from '../../../public/images/tictactoe.png';
+import website from '../../../public/images/logo.png';
+
 import './portfolio.scss';
 
 class Portfolio extends Component {
@@ -16,15 +23,54 @@ class Portfolio extends Component {
           </div>
         </div>
         <div className="portfolio-grid">
-          <div className="portfolio-grid-item">
-          hey
+          <div className="portfolio-grid-item cits">
+            <img src={cits} alt=""/>
+            <div className="project-hover">
+              <h4>cits.io</h4>
+              <a href="http://bit.ly/2GmmyEI" target="_blank">View Project</a>
+              <a href="https://github.com/thepicklefamily" target="_blank">View Code</a>
+            </div>
           </div>
-          <div className="portfolio-grid-item">
-          hey
+          <div className="portfolio-grid-item codesling">
+            <img src={codesling} alt=""/>
+            <div className="project-hover">
+              <h4>codesling.io</h4>
+              <a href="https://github.com/slyvanae" target="_blank">View Code</a>
+            </div>
           </div>
-          <div className="portfolio-grid-item">
-          hey
+          <div className="portfolio-grid-item bonebuds">
+            <img src={bonebuds} alt=""/>
+            <div className="project-hover">
+              <h4>BoneBuds</h4>
+              <a href="https://github.com/DangerDogs/BoneBuds" target="_blank">View Code</a>
+            </div>
           </div>
+          <div className="portfolio-grid-item rps">
+            <img src={rps} alt=""/>
+            <div className="project-hover">
+              <h4>rps</h4>
+              <a href="https://github.com/jaehchi/rps" target="_blank">View Code</a>
+            </div>
+          </div>
+          <div className="portfolio-grid-item tictactoe">
+            <img src={tictactoe} alt=""/>
+            <div className="project-hover">
+              <h4>tictactoe</h4>
+              <a href="https://github.com/jaehchi/ticTacToe" target="_blank">View Code</a>
+            </div>
+          </div>
+          <div className="portfolio-grid-item website">
+            <img src={website} alt=""/>
+            <div className="project-hover">
+              <h4>Website</h4>
+              <div className="current">
+                <a>Currently Viewing</a>
+              </div>
+              <a href="https://github.com/jaehchi/website" target="_blank">View Code</a>
+            </div>
+          </div>
+
+          
         </div>
       </div>
     );
