c0c7385c47f18c37d5d8973f0af55972141702b8
Jaehyun Chi <jaehchi@gmail.com>
2018-06-01 20:48:38 -0700

    [add] - adding compression

--- rest-server/src/config/express/index.js
+++ rest-server/src/config/express/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import parser from 'body-parser';
+import compression from 'compression';
 import { resolve } from 'path'
 
 const middleWare = [
+  compression(),
   helmet(),
   cors({
     allowedHeaders: ['Content-type', 'Authorization'],
